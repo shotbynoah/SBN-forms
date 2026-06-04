437790222935a355d56f8d07e94b3b9983acabee
Noah Kissinger <noah.kissinger24@gmail.com>
2026-06-03 18:28:04 -0700

    Fix canvas blocking inputs with pointer-events-none

--- components/ui/web-gl-shader.tsx
+++ components/ui/web-gl-shader.tsx
@@ -138,7 +138,7 @@ export function WebGLShader() {
   return (
     <canvas
       ref={canvasRef}
-      className="fixed top-0 left-0 w-full h-full block"
+      className="fixed top-0 left-0 w-full h-full block pointer-events-none"
     />
   )
 }
